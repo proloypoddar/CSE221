7847159558f15cf35d47f9bafdfcf05b3d835fe0
Polok Poddar <iamproloy9@gmail.com>
2023-06-06 01:13:26 +0600

    main

--- sort.py
+++ sort.py
@@ -1,4 +1,11 @@
 def selection(x):
-    n=len(x)
-    for i in range(0,n-1):
-        
+    if len(x)<=1:
+        return x
+    value=0
+    for i in range(1,len(x)):
+        if x[i]<x[value]:
+            value=i
+    x[0], x[value] = x[value], x[0]
+    return [x[0]]+selection(x[1:])
+z=[5,9,7,6,4]
+print(selection(z))
