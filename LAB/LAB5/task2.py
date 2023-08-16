23351623088ac136cb990e2123e4d4ff5bca95d8
POLOK PODDAR <iamproloy9@gmail.com>
2023-08-17 01:37:00 +0600

    main

--- LAB/LAB5/task2.py
+++ LAB/LAB5/task2.py
@@ -25,13 +25,13 @@ def Courses_number(N, P):
     if len(result) != N:
         return "IMPOSSIBLE"
     
-    return " ".join(map(str, result))
+    return (" ".join(map(str, result)))
 
 with open("F:\CSE221\LAB\LAB5\input_task2.txt", "r") as input_file:
     N, M = map(int, input_file.readline().split())
     P = [tuple(map(int, line.split())) for line in input_file]
 
-sequence = Courses_number(N, P)
+x = Courses_number(N, P)
 
 with open("F:\CSE221\LAB\LAB5\output_task2.txt", "w") as output_file:
-    output_file.write(sequence)
+    output_file.write(x)
